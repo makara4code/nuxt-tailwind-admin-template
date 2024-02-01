f8310ba3fc66d67aec780d8774e6b494f87d0015
Makara Nuol <42164455+makara4code@users.noreply.github.com>
2024-02-01 08:55:41 +0000

    commit the rest

---
+++ packages/eslint-config-custom/index.js
@@ -0,0 +1,44 @@
+require("@rushstack/eslint-patch/modern-module-resolution");
+
+module.exports = {
+  root: true,
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "plugin:json/recommended",
+    "@vue/eslint-config-prettier",
+  ],
+  roles: {
+    "vue/multi-word-component-names": "off",
+    "vue/valid-v-for": "off",
+    "linebreak-style": 0,
+    "antfu/if-newline": "off",
+    "vue/v-on-event-hyphenation": "off",
+    "no-console": "off",
+    "vue/no-setup-props-destructure": "off",
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+    ],
+  },
+  globals: {
+    queryContent: "readonly",
+    fetchContentNavigation: "readonly",
+    authenticate: "readonly",
+    navigationItem: "readonly",
+    useColorMode: "readonly",
+    availableBadges: "readonly",
+    useSchemaOrg: "readonly",
+  },
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    parser: "@typescript-eslint/parser",
+    sourceType: "module",
+  },
+  env: {
+    node: true,
+  },
+};
